297bc075242ff50009e0fd5723961f6485304099
Prashant Sahu <prashant.sahu_cs19@gla.ac.in>
2022-01-29 17:21:54 +0530

    second commit

--- infytq/question2.py
+++ infytq/question2.py
@@ -7,26 +7,27 @@
 # — — — — — — — — — — — -
 # Hello#81@21349
 # O/p : 984312
-def largest(str):
-    l = []
-    for i in str:
-        if(i.isdigit()):
-            if i not in l:
-                l.append(i)
-    l.sort(reverse=True)
-    for i in range( len(l)-1, 0, -1):
-        if(int(l[i]) % 2 == 0):
-            z = l[i]
-            l.remove(z)
-            l.append(z)
-            print("".join(l))
-            break
-    if i == 1:
-        print("-1")
-
-
 
 if __name__ == '__main__':
+    def largest(str1):
+        l1 = []
+        i = 0
+        for i in str1:
+            if i.isdigit():
+                if i not in l1:
+                    l1.append(i)
+        l1.sort(reverse=True)
+        for i in range(len(l1) - 1, 0, -1):
+            if int(l1[i]) % 2 == 0:
+                z = l1[i]
+                l1.remove(z)
+                l1.append(z)
+                print("".join(l1))
+                break
+        if i == 1:
+            print("-1")
+
+
     s1 = "infosys@337"
     s2 = "Hello#81@21349"
     largest(s1)
